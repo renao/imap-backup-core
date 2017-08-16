4dbcbdd451e7c64dfd991214db80d72ce257e97f
Renão <me@renao.de>
2017-08-16 22:53:17 +0200

    First quick and dirty MailKit implementation

---
+++ imap-backup-core/ImapDownloader.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using MailKit.Net.Imap;
+using MailKit;
+using MimeKit;
+using System.Collections.Generic;
+using MailKit.Search;
+using System.IO;
+
+namespace ImapBackup
+{
+    class ImapDownloader : IDisposable
+    {
+
+        ImapClient connection;
+
+        public ImapDownloader(
+            string username,
+            string password,
+            string host)
+        {
+            connection = new ImapClient();
+            connection.Connect(host, 993, true);
+            connection.Authenticate(username, password);
+        }
+
+        public void WriteAllMessagesToDisk(string basePath)
+        {
+            foreach (var folder in PersonalMailFolders)
+            {
+                folder.Open(FolderAccess.ReadOnly);
+                int count = folder.Count;
+                int progress = 0;
+                Console.WriteLine("Start download for folder: " + folder.FullName + " (" + count + " Messages in Total)");
+                string localFolderPath = Path.Combine(basePath, LocalFolderName(folder));
+                foreach (var uid in folder.Search(SearchQuery.All))
+                {
+                    progress += 1;
+                    Console.Write("\rSaving Mail " + progress + " from " + count);
+                        SaveEmail(localFolderPath, folder.GetMessage(uid));
+                }
+                Console.WriteLine(folder.FullName + " done.");
+            }
+        }
+
+        private void SaveEmail(string localFolderPath, MimeMessage message)
+        {
+            string emailFolderName = message.Date.ToString("yyyymmddhhmm");
+            string emailFolderPath = Path.Combine(localFolderPath, emailFolderName);
+
+            Directory.CreateDirectory(emailFolderPath);
+            message.WriteTo(Path.Combine(emailFolderPath, "content.eml"));
+
+
+            foreach (var attachment in message.Attachments)
+            {
+                var fileName = attachment.ContentDisposition?.FileName ?? attachment.ContentType.Name;
+
+                if (fileName == null) continue;
+
+
+                var localFileName = System.IO.Path.Combine(
+                    emailFolderPath,
+                    fileName);
+
+                using (var stream = System.IO.File.Create(localFileName))
+                {
+                    if (attachment is MessagePart)
+                    {
+                        var rfc822 = (MessagePart)attachment;
+                        rfc822.Message.WriteTo(stream);
+                    }
+                    else
+                    {
+                        var part = (MimePart)attachment;
+                        part.ContentObject.DecodeTo(stream);
+                    }
+                }
+            }
+
+        }
+
+        private IList<IMailFolder> PersonalMailFolders {
+            get {
+                return connection.GetFolders(connection.PersonalNamespaces[0]);
+            }
+        }
+
+        private string LocalFolderName(IMailFolder folder)
+        {
+            return folder.FullName.Replace(
+                folder.DirectorySeparator,
+                System.IO.Path.DirectorySeparatorChar);
+        }
+
+        public void Dispose()
+        {
+            if (connection?.IsConnected == true)
+            {
+                connection.Disconnect(true);
+                connection = null;
+            }
+        }
+    }
+}
--- imap-backup-core/Program.cs
+++ imap-backup-core/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using MailKit.Net.Imap;
+using MailKit;
+using MimeKit;
+using System.IO;
 
-namespace imap_backup_core
+namespace ImapBackup
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome. Please wait, starting IMAP-Backup");
+
+            Console.WriteLine("Enter your IMAP-username:");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Enter your IMAP-Password:");
+            string password = Console.ReadLine();
+
+            Console.WriteLine("Enter your IMAP-Hostadress:");
+            Console.WriteLine("(IMAP Backup will automatically start afterwards)");
+            string host = Console.ReadLine();
+
+            using (var downloader = new ImapDownloader(username, password, host))
+            {
+                downloader.WriteAllMessagesToDisk(Path.Combine(Environment.CurrentDirectory, "imap-backup"));
+            }
+
+            Console.WriteLine("Done. Press key to exit.");
+            Console.ReadKey();
         }
     }
 }
